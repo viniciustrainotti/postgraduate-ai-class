787bd2d1d7ec25a58bd4086c81c316e22fcf1aec
Vinícius Trainotti <vtrainotti@live.com>
2022-06-13 19:30:14 -0300

    add: script methods Lasso and Elasticnet

--- applied-statistics-II/exercise1.R
+++ applied-statistics-II/exercise1.R
@@ -70,25 +70,6 @@ test_dummies = predict(dummies, newdata = test[,cols_reg])
 
 print(dim(train_dummies)); print(dim(test_dummies))
 
-# A regress?o Ridge ? uma extens?o da regress?o linear em 
-# que a fun??o de perda ? modificada para minimizar a 
-# complexidade do modelo. Essa modifica??o ? feita 
-# adicionando um par?metro de penalidade equivalente ao 
-# quadrado da magnitude dos coeficientes.
-
-# Uma das principais diferen?as entre os modelos de regress?o
-# linear e regularizada ? que o ?ltimo envolve o ajuste de
-# um hiperpar?metro, lambda. O c?digo executa o modelo 
-# glmnet() v?rias vezes para diferentes valores de lambda. 
-# Podemos automatizar essa tarefa de encontrar o valor lambda
-# ideal usando a fun??o cv.glmnet(). Isso ? feito usando as
-# linhas de c?digo abaixo.
-
-# A fun??o perda ? dada por:
-# Loss function = OLS+lambda*summation(squared coefficient
-# values)
-# Lambda ? o par?metro de penalidade que selecionamos
-
 # The data for model
 
 x = as.matrix(train_dummies)
@@ -97,7 +78,8 @@ y_train = train$price
 x_test = as.matrix(test_dummies)
 y_test = test$price
 
-### The optimal lambda value ########
+# lambda
+
 lambdas <- 10^seq(2, -3, by = -.1)
 ridge_lamb <- cv.glmnet(x, y_train, alpha = 0, 
                         lambda = lambdas)
@@ -110,11 +92,12 @@ ridge_reg = glmnet(x, y_train, nlambda = 25, alpha = 0,
 
 summary(ridge_reg)
 
-# Obtendo os valores dos par?metros
+# Parameters
 
 ridge_reg[["beta"]]
 
 # Compute R^2 from true and predicted values
+
 eval_results <- function(true, predicted, df) {
   SSE <- sum((predicted - true)^2)
   SST <- sum((true - mean(true))^2)
@@ -130,22 +113,22 @@ eval_results <- function(true, predicted, df) {
 }
 
 # Prediction and evaluation on train data
+
 predictions_train <- predict(ridge_reg, s = best_lambda_ridge,
                              newx = x)
 eval_results(y_train, predictions_train, train)
 
 # Prediction and evaluation on test data
+
 predictions_test <- predict(ridge_reg, s = best_lambda_ridge, 
                             newx = x_test)
 eval_results(y_test, predictions_test, test)
 
 # Prediction for our example
 
-# Como os valores de entrada no modelo est?o normalizados,
-# temos de normalizar tamb?m os dados que queremos prever
-# Observe que as vari?veis dummies n?o sofrem normaliza??o
+# Ordenary value
 
-summary(dat)
+summary(dat) # Median 880000
 
 #Para: 880000
 # age = 9 anos
@@ -164,7 +147,7 @@ tarea = (183-pre_proc_val[["mean"]][["tarea"]])/
 bath = (3-pre_proc_val[["mean"]][["bath"]])/
   pre_proc_val[["std"]][["bath"]]
 
-# ensuit = 3
+# ensuit = 1
 ensuit = (1-pre_proc_val[["mean"]][["ensuit"]])/
   pre_proc_val[["std"]][["ensuit"]]
 
@@ -213,7 +196,7 @@ party = 1
 
 categ = 1
 
-# Construindo uma matriz com os dados para predi??o
+# Predict Matrix
 
 our_pred = as.matrix(data.frame(age=age,
                                 parea=parea,
@@ -234,15 +217,14 @@ our_pred = as.matrix(data.frame(age=age,
                                 fitg=fitg,
                                 party=party,
                                 categ=categ))
-# Fazendo a predi??o
+# Preditc 
 
 predict_our_ridge <- predict(ridge_reg, s = best_lambda_ridge, 
                              newx = our_pred)
 predict_our_ridge
 
-# O resultado ? uma informa??o normalizada, vamos 
-# convert?-la em valor nominal, compat?vel com a base de 
-# dados original
+# Ordenary value
+
 head(pre_proc_val)
 
 price_pred_ridge=(predict_our_ridge*pre_proc_val[["std"]][["price"]])+pre_proc_val[["mean"]][["price"]]
@@ -259,3 +241,126 @@ CIupr_ridge <- m - (qnorm(0.025))*dam
 
 CIlwr_ridge
 CIupr_ridge
+
+
+# Regressão Lasso
+
+# best lambda
+
+lambdas <- 10^seq(2, -3, by = -.1)
+
+# Setting alpha = 1 implements lasso regression
+lasso_lamb <- cv.glmnet(x, y_train, alpha = 1, 
+                        lambda = lambdas, 
+                        standardize = TRUE, nfolds = 5)
+
+# Best 
+best_lambda_lasso <- lasso_lamb$lambda.min 
+best_lambda_lasso
+
+lasso_model <- glmnet(x, y_train, alpha = 1, 
+                      lambda = best_lambda_lasso, 
+                      standardize = TRUE)
+
+# Parameters view
+
+lasso_model[["beta"]]
+
+# Predict train and test
+
+predictions_train <- predict(lasso_model, s = best_lambda_lasso,
+                             newx = x)
+eval_results(y_train, predictions_train, train)
+
+predictions_test <- predict(lasso_model, s = best_lambda_lasso, 
+                            newx = x_test)
+eval_results(y_test, predictions_test, test)
+
+## Prediction for our example
+
+# our_pred from Ridge model
+
+predict_our_lasso <- predict(lasso_model, s = best_lambda_lasso, 
+                             newx = our_pred)
+predict_our_lasso
+
+# Ordenary value
+
+price_pred_lasso=(predict_our_lasso*
+                   pre_proc_val[["std"]][["price"]])+
+  pre_proc_val[["mean"]][["price"]]
+price_pred_lasso
+
+# Confident intervals for our example
+
+n <- nrow(train)
+m <- price_pred_lasso
+s <- pre_proc_val[["std"]][["price"]]
+dam <- s/sqrt(n)
+CIlwr_lasso <- m + (qnorm(0.025))*dam
+CIupr_lasso <- m - (qnorm(0.025))*dam 
+
+CIlwr_lasso
+CIupr_lasso
+
+
+# Elasticnet
+
+# Set training control
+
+train_cont <- trainControl(method = "repeatedcv",
+                           number = 10,
+                           repeats = 5,
+                           search = "random",
+                           verboseIter = TRUE)
+
+# Train the model
+
+elastic_reg <- train(price ~ age+parea+tarea+bath+ensuit+garag+plaz+park+trans+
+                     kidca+school+health+bike+barb+balc+elev+fitg+party+categ,
+                     data = train,
+                     method = "glmnet",
+                     tuneLength = 10,
+                     trControl = train_cont)
+
+# Best tuning parameter
+
+elastic_reg$bestTune
+
+# And the parameters are:
+
+elastic_reg[["finalModel"]][["beta"]]
+
+# Make predictions on training set
+
+predictions_train <- predict(elastic_reg, x)
+eval_results(y_train, predictions_train, train) 
+
+# Make predictions on test set
+
+predictions_test <- predict(elastic_reg, x_test)
+eval_results(y_test, predictions_test, test)
+
+## Prediction for our example
+
+predict_our_elastic <- predict(elastic_reg,our_pred)
+predict_our_elastic
+
+# Ordenary value
+
+price_pred_elastic=(predict_our_elastic*
+                     pre_proc_val[["std"]][["price"]])+
+  pre_proc_val[["mean"]][["price"]]
+price_pred_elastic
+
+# Confident intervals for our example
+
+n <- nrow(train)
+m <- price_pred_elastic
+s <- pre_proc_val[["std"]][["price"]]
+dam <- s/sqrt(n)
+CIlwr_elastic <- m + (qnorm(0.025))*dam
+CIupr_elastic <- m - (qnorm(0.025))*dam 
+
+CIlwr_elastic
+CIupr_elastic
